6840b69b500ba34deb9f5034b5a60bf86363861f
AfsheenTabassum <71068046+AfsheenTabassum@users.noreply.github.com>
2020-10-13 12:44:37 +0530

    product.cpp

---
+++ product.cpp
@@ -0,0 +1,19 @@
+//Program to find the product of two numbers entered by the user
+int main()
+{
+	
+	double a,b,product;
+	cout<<"Enter two numbers"<<endl;
+	cin>>a>>b;
+	product=a*b;
+	cout<<"product="<<product;
+	
+	return 0;
+	
+}    
+
+Output:
+Enter two numbers
+2.2 5.1
+product= 11.22
+
